ffd36f4e86f0059afd750af9c41a812dc4df14d2
antonio-pedro99 <tonio.pedro99@gmail.com>
2023-01-11 12:50:38 +0530

    Step 2 - setup game

--- D1 - Doodle Dash/lib/main.dart
+++ D1 - Doodle Dash/lib/main.dart
@@ -1,4 +1,14 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'game/doodle_dash.dart';
+import 'game/util/util.dart';
+import 'game/widgets/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,24 +16,30 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Doodle Dash',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: lightColorScheme,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorScheme: darkColorScheme,
+        textTheme: GoogleFonts.audiowideTextTheme(ThemeData.dark().textTheme),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Doodle Dash'),
     );
   }
 }
 
+final Game game = DoodleDash();
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -32,10 +48,25 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Container(
+            constraints: const BoxConstraints(
+              maxWidth: 800,
+              minWidth: 550,
+            ),
+            child: GameWidget(
+              game: game,
+              overlayBuilderMap: <String, Widget Function(BuildContext, Game)>{
+                'gameOverlay': (context, game) => GameOverlay(game),
+                'mainMenuOverlay': (context, game) => MainMenuOverlay(game),
+                'gameOverOverlay': (context, game) => GameOverOverlay(game),
+              },
+            ),
+          );
+        }),
       ),
-      body: Container(),
     );
   }
 }
